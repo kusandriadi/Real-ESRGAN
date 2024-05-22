656e01eca04493cad7df42281a8604196d87d1af
Kus Andriadi <me@kusandriadi.com>
2024-05-22 16:47:37 +0700

    upgrade torch and adding torch profiler

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-import os 
+import os
+
 import torch
-import torchvision.models as models
-from torch.profiler import profile, record_function, ProfilerActivity
-import time
-import psutil
 from PIL import Image
-import numpy as np
+from torch.profiler import ProfilerActivity
+
 from RealESRGAN import RealESRGAN
+
+weight = 'x2'
+scale = 2
 
 def main() -> int:
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = RealESRGAN(device, scale=8)
-    model.load_weights('weights/RealESRGAN_x8.pth', download=True)
+    model = RealESRGAN(device, scale=scale)
+    model.load_weights(f'weights/RealESRGAN_{weight}.pth', download=True)
 
     with torch.profiler.profile(
             activities=[
@@ -20,10 +21,11 @@ def main() -> int:
             profile_memory=True, record_shapes=True) as prof:
 
         for i, image in enumerate(os.listdir("inputs")):
+            print(f'Gambar ke {i}')
             image_name, _ = os.path.splitext(image)
             image = Image.open(f"inputs/{image}").convert('RGB')
             sr_image = model.predict(image)
-            sr_image.save(f'results/{image_name}' + '_result_x8.png')
+            sr_image.save(f'results/{image_name}' + f'_result_{weight}_{scale}.png')
 
     # Print profiling results
     print(prof.key_averages().table(sort_by="cpu_time_total", row_limit=10))
